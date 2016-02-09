a2e053c43e951a53a769b9070e69505e76bd02c6
Miguel Méndez <mmendez@binarioweb.com>
2016-02-09 15:32:27 -0600

    division de elementos

---
+++ banners.php
@@ -0,0 +1,21 @@
+<!--            Banners-->
+<div class="row">
+    <div class="col-md-1"></div>
+    <div class="col-md-10 bloques">
+
+        <div class="col-md-12">
+            <div id="ninja-slider">
+                <div class="slider-inner">
+                    <ul>
+                        <li><img class="ns-img" src="imgs/a.jpg" /></li>
+                        <li><img class="ns-img" src="imgs/a.jpg" /></li>
+                        <li><img class="ns-img" src="imgs/a.jpg" /></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <div class="col-md-1"></div>
+</div>
+<!--                Fin banners-->
---
+++ footer.php
@@ -0,0 +1,37 @@
+<footer class="row grupoha-footer"><!--Aquí inicia footer--> 
+                <div class="col-md-1"></div>
+                <div class="col-md-10 bloques footer">
+                    <nav id="menu" class="nav">					
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    <span>Inicio</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>Servicios</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>Enlaces comerciales</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>Contacto</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                    <div class="col-md-6 text-right aviso"><p>Grupo Ha’ 2016<br/>Derechos Reservados<br/><a id="aviso-privacidad" href="#">Aviso de privacidad</a></p></div>
+                    <div class="col-md-6"><p class="text-left">(55) 5606 5167</p>
+                        <ul id= "iconos2">
+                            <li id="facebook"><a href="https://www.facebook.com/grupoha.stillman" title="Síguenos en Facebook"></a></li>
+                            <li id="twitter"><a href="https://twitter.com/GrupoHa" title="Síguenos en Twitter"></a> </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-1"></div>
+            </footer><!--Aquí termina footer-->
